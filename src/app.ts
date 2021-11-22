7ce863bd788b4a731992b34a927295bb92d89d23
backendprincess <lemur.king.julien@gmail.com>
2021-11-22 16:22:10 +0200

    Add ListTemplate class

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
@@ -8,6 +9,9 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
@@ -19,5 +23,6 @@ form.addEventListener('submit', (e: Event) => {
     } else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    
+    list.render(doc, type.value, 'end');
 })
